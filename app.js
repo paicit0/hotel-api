385478d72a2600816dcfd35c628f740cf65f2c65
Piyapat <piyapat.pai@outlook.com>
2025-08-31 01:20:39 +0700

    init hotel-api

---
+++ app.js
@@ -0,0 +1,16 @@
+// node app.js
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json({hotelID: '1234'});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
