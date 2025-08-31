b7a964caa78f213767d9cc1dfdaaea4b11e87891
Piyapat <piyapat.pai@outlook.com>
2025-08-31 22:26:07 +0700

    Refactor hotel API endpoints and add search functionality

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-// node app.js
+// node --watch app.js
 
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8080;
 
+const mockHotelData = require('./mockHotelData.json');
+
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.json({hotelID: '1234'});
+app.get('/search', (req, res) => {
+  const { location } = req.query;
+  console.log(`Received request for hotel location: ${location}`);
+  const filteredHotels = mockHotelData.filter(hotel => hotel.hotel_location.toLowerCase().includes(location.toLowerCase()));
+  console.log(filteredHotels);
+  res.json(filteredHotels);
+});
+
+app.get('/getHotelById', (req, res) => {
+  const { id } = req.query;
+  console.log(`Received request for hotel ID: ${id}`);
+  if (id === undefined) {
+    res.status(400).json({ message: 'Hotel ID is required' });
+    return;
+  }
+  const filteredHotel = mockHotelData.find(hotel => hotel.hotel_id === id);
+  if (filteredHotel === undefined) {
+    res.status(404).json({ message: `Hotel with ID ${id} not found` });
+  } else {
+    console.log([filteredHotel]);
+    res.json([filteredHotel]);
+  }
 });
 
 app.listen(port, () => {
